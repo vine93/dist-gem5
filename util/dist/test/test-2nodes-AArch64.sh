f185adb773adfdc11bf1aaa308cf2f35f9281447
vine93 <vineetha.govindaraj@gmail.com>
2019-10-07 17:33:24 +0000

    Changed the assosiativity

--- util/dist/test/test-2nodes-AArch64.sh
+++ util/dist/test/test-2nodes-AArch64.sh
@@ -62,7 +62,7 @@ DEBUG_FLAGS="--debug-flags=DistEthernet,Ethernet,WorkItems,TimingIdle,CacheFlush
 #CHKPT_RESTORE="-r1"
 CKPTDIR=$(pwd)
 RUNDIR=$(pwd)/rundir
-NNODES=2
+NNODES=3
 
 $GEM5_DIST_SH -n $NNODES                                                     \
               -x $GEM5_EXE                                                   \
@@ -80,13 +80,13 @@ $GEM5_DIST_SH -n $NNODES                                                     \
                   --script=$BOOT_SCRIPT                                      \
                   --l1d_size=32kB					     \
                   --l1i_size=32kB					     \
-		  --l2_size=512kB  					     \
+		  --l2_size=256kB  					     \
 		  --l3_size=16MB  					     \
-		  --l1d_assoc=8    						\
-		  --l1i_assoc=8							\
+		  --l1d_assoc=2    						\
+		  --l1i_assoc=2							\
 		  --l2_assoc=4							\
-		  --privateL2cache						\
 		  --l3cache							\
+		  --privateL2cache						\
 		  --caches                                                   \ 
               --cf-args				  			     \
 		  --$CHKPT_RESTORE						\
@@ -94,4 +94,5 @@ $GEM5_DIST_SH -n $NNODES                                                     \
 
 #	      -c $CKPTDIR						     \
 #	      -r $RUNDIR						     \
+
 
