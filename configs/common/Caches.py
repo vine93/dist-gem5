8f5b9c0b67ecd5c4bfbd7a76d200eadde70b88c8
vine93 <vineetha.govindaraj@gmail.com>
2019-10-14 00:56:46 +0000

    added L3 XBAr

--- configs/common/Caches.py
+++ configs/common/Caches.py
@@ -73,13 +73,13 @@ class L2Cache(Cache):
     write_buffers = 8
 
 class L3Cache(Cache):
-    assoc = 8
-    tag_latency = 25 
-    data_latency = 25
-    response_latency = 25
-    mshrs = 20
-    tgts_per_mshr = 12
-    write_buffers = 8
+    assoc = 64
+    tag_latency = 32 
+    data_latency = 32
+    response_latency = 32
+    mshrs = 32
+    tgts_per_mshr = 24
+    write_buffers = 16
 
 #class L4Cache(TileCache):
 #    assoc = 8
